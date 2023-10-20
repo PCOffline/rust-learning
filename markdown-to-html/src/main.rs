77cff9dfa282feff8c8d3659d918e40768e333a5
Eldar Bakerman <eldar@tictuk.com>
2023-10-20 22:36:25 +0300

    feat: move "over-engineered" code to different file

--- markdown-to-html/src/main.rs
+++ markdown-to-html/src/main.rs
@@ -1,189 +1,44 @@
-use regex::escape;
 use regex::Regex;
 use regex::RegexBuilder;
+use std::env::args;
 use std::fs;
 use std::io;
-use std::usize;
+mod over_engineered;
 
 fn main() -> io::Result<()> {
     let mut input = String::new();
     let stdin = io::stdin();
 
+    // TODO: Use over-engineered version if true
+    let use_over_engineered_version = args()
+        .nth(1)
+        .and_then(|arg| Some(arg == "--over_engineered" || arg == "-oe"))
+        .unwrap_or(false);
+
     println!("Enter the path to the markdown file:");
     stdin.read_line(&mut input)?;
 
     let content_result = fs::read_to_string(&input.trim())?;
-    let html = parse_file_to_html(&content_result);
+    let mut html;
+
+    if (use_over_engineered_version) {
+        html = over_engineered::parse_markdown_to_html(&content_result);
+    } else {
+        html = parse_markdown_to_html(&content_result);
+    }
 
     write_html_file(&input, &html)?;
 
     Ok(())
 }
 
-enum Element<'a> {
-    Title(&'a str),
-    Subtitle(&'a str),
-    Header(&'a str),
-    Subheader(&'a str),
-    Bold(&'a str),
-    Italic(&'a str),
-    Strikethrough(&'a str),
-    Underline(&'a str),
-    Code(&'a str),
-    Link(&'a str, &'a str),
-    Image(&'a str, &'a str),
-    UnorderedList(Vec<&'a str>),
-    OrderedList(Vec<&'a str>),
-    Checkbox(bool, &'a str),
-    PlainText(&'a str),
-}
-
-impl<'a> Element<'a> {
-    fn to_html(&self) {
-        match self {
-            Self::Title(text) => println!("Title: {text}"),
-            Self::Subtitle(text) => println!("Subtitle: {text}"),
-            Self::Header(text) => println!("Header: {text}"),
-            Self::Subheader(text) => println!("Subheader: {text}"),
-            Self::Bold(text) => println!("Bold: {text}"),
-            Self::Italic(text) => println!("Italic: {text}"),
-            Self::Strikethrough(text) => println!("Strikethrough: {text}"),
-            Self::Underline(text) => println!("Underline: {text}"),
-            Self::Code(text) => println!("Code: {text}"),
-            Self::OrderedList(vec) => println!("OrderedList: {:?}", vec),
-            Self::UnorderedList(vec) => println!("UnorderedList: {:?}", vec),
-            Self::Checkbox(checked, text) => {
-                println!("OrderedList: {{ checked: {checked}; text: {text} }}")
-            }
-            Self::Link(text, url) => println!("Link: {{ text: {text}; url: {url} }}"),
-            Self::Image(url, alt) => println!("Image: {{ url: {url}; alt: {alt} }}"),
-            Self::PlainText(text) => println!("PlainText: {text}"),
-        }
-    }
-
-    fn from(text: &str) -> Element {
-        fn gen_regex_str_for_wrap(template: &str) -> String {
-            escape(template) + r"[^" + template + "].*" + &escape(template)
-        }
-
-        fn gen_regex_str_for_prefix(template: &str) -> String {
-            "^".to_owned() + &escape(template) + "[^" + template + "].+$"
-        }
-
-        let title_regex = Regex::new(&gen_regex_str_for_prefix("#")).unwrap();
-        let subtitle_regex = Regex::new(&gen_regex_str_for_prefix("##")).unwrap();
-        let header_regex = Regex::new(&gen_regex_str_for_prefix("###")).unwrap();
-        let subheader_regex = Regex::new(&gen_regex_str_for_prefix("####")).unwrap();
-
-        let italic_regex = Regex::new(&gen_regex_str_for_wrap(r"\*")).unwrap();
-        let bold_regex = Regex::new(&gen_regex_str_for_wrap(r"\*\*")).unwrap();
-        let strikethrough_regex = Regex::new(&gen_regex_str_for_wrap("~")).unwrap();
-        let underline_regex = Regex::new(&gen_regex_str_for_wrap("_")).unwrap();
-
-        let unordered_list_regex = Regex::new(r"^\s*-\s+.+$").unwrap();
-        let ordered_list_regex = Regex::new(r"^\s*\d\.\s.+$").unwrap();
-        let checkbox_regex = Regex::new(r"^\s*\[[xX]?\]\s(.+)$").unwrap();
-        let link_regex = Regex::new(r"\[(?<text>.+)\]\((?<url>.+)\)").unwrap();
-        let image_regex = Regex::new(r"\!\[(?<alt>.+)\]\((?<url>.+)\)").unwrap();
-        let code_regex = Regex::new(&gen_regex_str_for_wrap("`")).unwrap();
-        let code_block_regex = Regex::new(&gen_regex_str_for_wrap(r"\n```\n")).unwrap();
-
-        fn get_trimmed_text<'a>(text: &'a str, pattern: &'a str) -> &'a str {
-            let index_of_first_char: usize = text
-                .find(pattern)
-                .and_then(|index| Some(index + pattern.len()))
-                .unwrap_or(0);
-
-            &text[index_of_first_char..]
-        }
-
-        fn get_trimmed_text_from_both_sides<'a>(text: &'a str, pattern: &'a str) -> &'a str {
-            let index_of_first_char: usize = text
-                .find(pattern)
-                .and_then(|index| Some(index + pattern.len()))
-                .unwrap_or(0);
-
-            let index_of_last_char: usize = text.rfind(pattern).unwrap_or(text.len());
-
-            &text[index_of_first_char..index_of_last_char]
-        }
-
-        if title_regex.is_match(text) {
-            return Element::Title(get_trimmed_text(text, "# "));
-        } else if subtitle_regex.is_match(text) {
-            return Element::Subtitle(get_trimmed_text(text, "## "));
-        } else if header_regex.is_match(text) {
-            return Element::Header(get_trimmed_text(text, "### "));
-        } else if subheader_regex.is_match(text) {
-            return Element::Subheader(get_trimmed_text(text, "#### "));
-        } else if bold_regex.is_match(text) {
-            return Element::Bold(get_trimmed_text_from_both_sides(text, "**"));
-        } else if italic_regex.is_match(text) {
-            return Element::Italic(get_trimmed_text_from_both_sides(text, "*"));
-        } else if strikethrough_regex.is_match(text) {
-            return Element::Strikethrough(get_trimmed_text_from_both_sides(text, "~"));
-        } else if underline_regex.is_match(text) {
-            return Element::Underline(get_trimmed_text_from_both_sides(text, "_"));
-        } else if code_regex.is_match(text) {
-            return Element::Code(get_trimmed_text_from_both_sides(text, "`"));
-        } else if code_block_regex.is_match(text) {
-            return Element::Code(get_trimmed_text_from_both_sides(text, "```"));
-        } else if checkbox_regex.is_match(text) {
-            let checked: bool = text
-                .trim()
-                .chars()
-                .nth(1)
-                .unwrap()
-                .to_string()
-                .to_lowercase()
-                == "x";
-            let checkbox_text = checkbox_regex
-                .captures(text)
-                .unwrap()
-                .get(1)
-                .unwrap()
-                .as_str();
-
-            return Element::Checkbox(checked, checkbox_text);
-        } else if link_regex.is_match(text) {
-            let captures = link_regex.captures(text).unwrap();
-            return Element::Link(
-                &captures.name("text").unwrap().as_str(),
-                &captures.name("url").unwrap().as_str(),
-            );
-        } else if image_regex.is_match(text) {
-            let captures = image_regex.captures(text).unwrap();
-            return Element::Image(
-                &captures.name("alt").unwrap().as_str(),
-                &captures.name("url").unwrap().as_str(),
-            );
-        } else if text.lines().all(|line| unordered_list_regex.is_match(line)) {
-            return Element::UnorderedList(
-                text.lines()
-                    .enumerate()
-                    .map(|(_, line)| get_trimmed_text(&line, "- "))
-                    .collect::<Vec<&str>>(),
-            );
-        } else if text.lines().all(|line| ordered_list_regex.is_match(line)) {
-            return Element::OrderedList(
-                text.lines()
-                    .enumerate()
-                    .map(|(_, line)| get_trimmed_text(&line, ". "))
-                    .collect::<Vec<&str>>(),
-            );
-        }
-
-        Element::PlainText(text)
-    }
-}
-
-fn parse_file_to_html(content: &str) -> String {
+fn parse_markdown_to_html(content: &str) -> String {
     fn gen_regex(pattern: &str) -> Regex {
         RegexBuilder::new(pattern)
-        .multi_line(true)
-        .case_insensitive(true)
-        .build()
-        .unwrap()
+            .multi_line(true)
+            .case_insensitive(true)
+            .build()
+            .unwrap()
     }
 
     let title_regex = gen_regex(r"^#\s(.+)$");
@@ -205,18 +60,38 @@ fn parse_file_to_html(content: &str) -> String {
     let code_block_regex = gen_regex(r"^\s*```\w*\n((.+\n*)+)\n```$");
 
     let content = title_regex.replace_all(content, "<h1>$1</h1>").to_string();
-    let content = subtitle_regex.replace_all(&content, "<h2>$1</h2>").to_string();
-    let content = subtitle_regex.replace_all(&content, "<h2>$1</h2>").to_string();
-    let content = header_regex.replace_all(&content, "<h3>$1</h3>").to_string();
-    let content = subheader_regex.replace_all(&content, "<h4>$1</h4>").to_string();
+    let content = subtitle_regex
+        .replace_all(&content, "<h2>$1</h2>")
+        .to_string();
+    let content = subtitle_regex
+        .replace_all(&content, "<h2>$1</h2>")
+        .to_string();
+    let content = header_regex
+        .replace_all(&content, "<h3>$1</h3>")
+        .to_string();
+    let content = subheader_regex
+        .replace_all(&content, "<h4>$1</h4>")
+        .to_string();
     let content = bold_regex.replace_all(&content, "<b>$1</b>").to_string();
     let content = italic_regex.replace_all(&content, "<i>$1</i>").to_string();
-    let content = strikethrough_regex.replace_all(&content, "<s>$1</s>").to_string();
-    let content = underline_regex.replace_all(&content, "<u>$1</u>").to_string();
-    let content = code_block_regex.replace_all(&content, "<pre><code>$1</code><pre>").to_string();
-    let content = code_regex.replace_all(&content, "<code>$1</code>").to_string();
-    let content = image_regex.replace_all(&content, "<img src=\"$url\" alt=\"$alt\">").to_string();
-    let content = link_regex.replace_all(&content, "<a href=\"$url\">$text</a>").to_string();
+    let content = strikethrough_regex
+        .replace_all(&content, "<s>$1</s>")
+        .to_string();
+    let content = underline_regex
+        .replace_all(&content, "<u>$1</u>")
+        .to_string();
+    let content = code_block_regex
+        .replace_all(&content, "<pre><code>$1</code><pre>")
+        .to_string();
+    let content = code_regex
+        .replace_all(&content, "<code>$1</code>")
+        .to_string();
+    let content = image_regex
+        .replace_all(&content, "<img src=\"$url\" alt=\"$alt\">")
+        .to_string();
+    let content = link_regex
+        .replace_all(&content, "<a href=\"$url\">$text</a>")
+        .to_string();
     // let content = subtitle_regex.replace_all(&content, "<h2>$1</h2>").to_string();
     // let content = subtitle_regex.replace_all(&content, "<h2>$1</h2>").to_string();
     // let content = subtitle_regex.replace_all(&content, "<h2>$1</h2>").to_string();
---
+++ markdown-to-html/src/over_engineered.rs
@@ -0,0 +1,115 @@
+use regex::Captures;
+use regex::Regex;
+use regex::RegexBuilder;
+
+enum Element<'a> {
+    Title(&'a str),
+    Subtitle(&'a str),
+    Header(&'a str),
+    Subheader(&'a str),
+    Bold(&'a str),
+    Italic(&'a str),
+    Strikethrough(&'a str),
+    Underline(&'a str),
+    Code(&'a str),
+    CodeBlock(&'a str),
+    Link(&'a str, &'a str),
+    Image(&'a str, &'a str),
+    UnorderedList(Vec<&'a str>),
+    OrderedList(Vec<&'a str>),
+    Checkbox(bool, &'a str),
+}
+
+impl<'a> Element<'a> {
+    fn to_html(&self) -> String {
+        match self {
+            Self::Title(text) => format!("<h1>{text}</h1>"),
+            Self::Subtitle(text) => format!("<h2>{text}</h2>"),
+            Self::Header(text) => format!("<h3>{text}</h3>"),
+            Self::Subheader(text) => format!("<h4>{text}</h4>"),
+            Self::Bold(text) => format!("<b>{text}</b>"),
+            Self::Italic(text) => format!("<i>{text}</i>"),
+            Self::Strikethrough(text) => format!("<s>{text}</s>"),
+            Self::Underline(text) => format!("<u>{text}</u>"),
+            Self::CodeBlock(text) => format!("<pre><code>{text}</code></pre>"),
+            Self::Code(text) => format!("<code>{text}</code>"),
+            Self::OrderedList(vec) => format!(
+                "<ol>{}</ol>",
+                vec.iter()
+                    .map(|item| format!("<li>{item}</li>"))
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            ),
+            Self::UnorderedList(vec) => format!(
+                "<ul>{}</ul>",
+                vec.iter()
+                    .map(|item| format!("<li>{item}</li>"))
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            ),
+            Self::Checkbox(checked, text) => {
+                format!("<input type=\"checkbox\" checked=\"{checked}\"><label>{text}</label>")
+            }
+            Self::Link(text, url) => format!("<a href=\"{url}\">{text}</a>"),
+            Self::Image(alt, url) => format!("<img src=\"{url}\" alt=\"{alt}\">"),
+        }
+    }
+}
+
+pub fn parse_markdown_to_html(text: &str) -> String {
+    fn gen_regex(pattern: &str) -> Regex {
+        RegexBuilder::new(pattern)
+            .multi_line(true)
+            .case_insensitive(true)
+            .build()
+            .unwrap()
+    }
+
+    fn gen_prefix_regex(pattern: &str) -> Regex {
+        gen_regex(&(String::from(r"^\s*") + pattern + r"(?<text>.+)$"))
+    }
+
+    fn gen_wrap_regex(pattern: &str) -> Regex {
+        gen_regex(&(pattern.to_owned() + r"(?<text>.+)" + pattern))
+    }
+
+    fn list_str_to_vec(list_str: &str) -> Vec<&str> {
+        list_str.split("\n").filter(|str| str.trim() != "").map(|str| &str.trim()[str.find(' ').unwrap() + 1..]).collect::<Vec<&str>>()
+    }
+
+    let title_regex = gen_prefix_regex("#");
+    let subtitle_regex = gen_prefix_regex("##");
+    let header_regex = gen_prefix_regex("###");
+    let subheader_regex = gen_prefix_regex("####");
+
+    let italic_regex = gen_wrap_regex(r"\*");
+    let bold_regex = gen_wrap_regex(r"\*\*");
+    let strikethrough_regex = gen_wrap_regex("~");
+    let underline_regex = gen_wrap_regex("_");
+
+    let unordered_list_regex = gen_regex(r"(^\s*-\s.+\n*)+");
+    let ordered_list_regex = gen_regex(r"(^\s*\d+\.\s.+\n*)+");
+    let checkbox_regex = gen_prefix_regex(r"\[(?<checkmark>x?)\]\s");
+    let link_regex = gen_regex(r"\[(?<text>.+)\]\((?<url>.+)\)");
+    let image_regex = gen_regex(r"\!\[(?<alt>.*)\]\((?<url>.+)\)");
+    let code_regex = gen_wrap_regex("`");
+    let code_block_regex = gen_regex(r"^\s*```\w*\n(?<text>(.+\n*)+)\n```\s*$");
+
+    let text = title_regex.replace_all(text, |captures: &Captures| Element::Title(captures.name("text").unwrap().as_str()).to_html());
+    let text = subtitle_regex.replace_all(&text, |captures: &Captures| Element::Subtitle(captures.name("text").unwrap().as_str()).to_html());
+    let text = header_regex.replace_all(&text, |captures: &Captures| Element::Header(captures.name("text").unwrap().as_str()).to_html());
+    let text = subheader_regex.replace_all(&text, |captures: &Captures| Element::Subheader(captures.name("text").unwrap().as_str()).to_html());
+    let text = bold_regex.replace_all(&text, |captures: &Captures| Element::Bold(captures.name("text").unwrap().as_str()).to_html());
+    let text = italic_regex.replace_all(&text, |captures: &Captures| Element::Italic(captures.name("text").unwrap().as_str()).to_html());
+    let text = strikethrough_regex.replace_all(&text, |captures: &Captures| Element::Strikethrough(captures.name("text").unwrap().as_str()).to_html());
+    let text = underline_regex.replace_all(&text, |captures: &Captures| Element::Underline(captures.name("text").unwrap().as_str()).to_html());
+    let text = code_block_regex.replace_all(&text, |captures: &Captures| Element::CodeBlock(captures.name("text").unwrap().as_str()).to_html());
+    let text = code_regex.replace_all(&text, |captures: &Captures| Element::Code(captures.name("text").unwrap().as_str()).to_html());
+    let text = image_regex.replace_all(&text, |captures: &Captures| Element::Image(captures.name("alt").unwrap().as_str(), captures.name("url").unwrap().as_str()).to_html());
+    let text = link_regex.replace_all(&text, |captures: &Captures| Element::Link(captures.name("text").unwrap().as_str(), captures.name("url").unwrap().as_str()).to_html());
+    let text = checkbox_regex.replace_all(&text, |captures: &Captures| Element::Checkbox(captures.name("checkmark").unwrap().as_str() != "", captures.name("text").unwrap().as_str()).to_html());
+    let text = unordered_list_regex.replace_all(&text, |captures: &Captures| Element::UnorderedList(list_str_to_vec(captures.get(0).unwrap().as_str())).to_html());
+    let text = ordered_list_regex.replace_all(&text, |captures: &Captures| Element::OrderedList(list_str_to_vec(captures.get(0).unwrap().as_str())).to_html());
+
+    text.to_string()
+}
